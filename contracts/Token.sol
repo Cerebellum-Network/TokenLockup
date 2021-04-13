db0207e747e9668bb4371271c1a8d677e067710d
Noah Thorp <noah@comakery.com>
2021-04-12 20:48:29 -0700

    Remove Ownable from Token

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
-contract Token is ERC20, Ownable {
+contract Token is ERC20 {
 
     using SafeMath for uint256;
 
