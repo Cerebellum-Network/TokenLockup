ccc72ca84eda405db57f9234315e3e6af549432b
Noah Thorp <noah@comakery.com>
2021-04-20 01:32:50 -0700

    can approve a transfer by another account

---
+++ test/token-release-scheduler/approve-and-transfer-from.js
@@ -0,0 +1,75 @@
+const hre = require('hardhat')
+const chai = require('chai')
+const { expect } = chai
+const { solidity } = require('ethereum-waffle')
+chai.use(solidity)
+
+async function currentTimestamp (offsetInSeconds = 0) {
+  return (await hre.ethers.provider.getBlock()).timestamp + offsetInSeconds
+}
+
+async function exactlyMoreThanOneDayAgo () {
+  return await currentTimestamp(-3601)
+}
+
+describe('TokenReleaseScheduler unlock scheduling', async function () {
+  let releaser, token, reserveAccount, recipient, accounts
+  const decimals = 10
+  const totalSupply = 8e9
+
+  beforeEach(async () => {
+    accounts = await hre.ethers.getSigners()
+
+    reserveAccount = accounts[0]
+    recipient = accounts[1]
+
+    const Token = await hre.ethers.getContractFactory('Token')
+
+    token = await Token.deploy(
+      'Test Scheduled Release Token',
+      'SCHR',
+      decimals,
+      totalSupply,
+      [accounts[0].address],
+      [totalSupply]
+    )
+    const TokenReleaseScheduler = await hre.ethers.getContractFactory('TokenReleaseScheduler')
+    releaser = await TokenReleaseScheduler.deploy(
+      token.address,
+      'Xavier Yolo Zeus Token Lockup Release Scheduler',
+      'XYZ Lockup',
+      30 // low minimum to force rounding issues
+    )
+  })
+
+  it('unlocked tokens can be transferred by an approved account with balances from multiple lockups', async () => {
+    const totalRecipientAmount = 100
+    const totalBatches = 3
+    const firstDelay = 0
+    const firstBatchBips = 800 // 8%
+    const batchDelay = 3600 * 24 * 4 // 4 days
+    const commence = await exactlyMoreThanOneDayAgo()
+    const allowedAccount = accounts[3]
+    const allowedAccountRecipient = accounts[4]
+
+    await token.connect(reserveAccount).approve(releaser.address, totalRecipientAmount)
+
+    await releaser.connect(reserveAccount).createReleaseSchedule(
+      totalBatches,
+      firstDelay,
+      firstBatchBips,
+      batchDelay
+    )
+
+    await releaser.connect(reserveAccount).fundReleaseSchedule(
+      recipient.address,
+      100,
+      commence,
+      0 // scheduleId
+    )
+
+    expect(await releaser.connect(recipient).balanceOf(recipient.address)).to.equal(100)
+    await releaser.connect(recipient).approve(allowedAccount.address, 7)
+    await releaser.connect(allowedAccount).transferFrom(recipient.address, allowedAccountRecipient.address, 7)
+  })
+})
