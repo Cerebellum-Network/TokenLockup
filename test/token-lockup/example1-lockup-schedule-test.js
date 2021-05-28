045dae21d354b5d6a69ebbd2cbae55d50e26ce97
Noah Thorp <noah@comakery.com>
2021-05-28 15:07:21 -0700

    Move release schedule example code into its own module

--- test/token-lockup/example1-lockup-schedule-test.js
+++ test/token-lockup/example1-lockup-schedule-test.js
@@ -4,6 +4,12 @@ const { expect } = chai
 const { solidity } = require('ethereum-waffle')
 chai.use(solidity)
 
+function days (numDays) {
+  return 60 * 60 * 24 * numDays
+}
+
+const createReleaseSchedule = require('../../lib/createReleaseScheduleExample1')
+
 describe('test lockup periods for token release example', async function () {
   let tokenLockup, token, reserveAccount
   const decimals = 10
@@ -11,7 +17,6 @@ describe('test lockup periods for token release example', async function () {
 
   beforeEach(async () => {
     const accounts = await hre.ethers.getSigners()
-
     reserveAccount = accounts[0]
 
     const Token = await hre.ethers.getContractFactory('Token')
@@ -41,41 +46,27 @@ describe('test lockup periods for token release example', async function () {
   })
 
   it('has the expected release schedules, count and ids', async function () {
-    // 0: 7.7% unlocked at distribution, the rest vesting in equal portions every 90 days for 360 days
-    let ninetyDaysInSeconds = 60 * 60 * 24 * 90 // 7776000
-
-    await tokenLockup.connect(reserveAccount).createReleaseSchedule(
-      5, // release count including initial
-      0, // delay till first release
-      770, // initial release portion in Bips 100ths of 1%
-      ninetyDaysInSeconds // period between releases in seconds
-    )
-
-    // 1: 7.2% unlocked at distribution, the rest vesting in equal portions every 90 days for 540 days
-    await tokenLockup.connect(reserveAccount).createReleaseSchedule(
-      7, // release count including initial
-      0, // delay till first release
-      720, // initial release portion in Bips 100ths of 1%
-      ninetyDaysInSeconds // period between releases in seconds
-    )
-
-    // 2: 20% unlocked at distribution, the rest vesting in equal portions every 90 days for 360 days
-    await tokenLockup.connect(reserveAccount).createReleaseSchedule(
-      5, // release count including initial
-      0, // delay till first release
-      2000, // initial release portion in Bips 100ths of 1%
-      ninetyDaysInSeconds // period between releases in seconds
-    )
-
-    // 3: 25% unlocked at distribution, the rest vesting in equal portions every 90 days for 360 days
-
-    await tokenLockup.connect(reserveAccount).createReleaseSchedule(
-      5, // release count including initial
-      0, // delay till first release
-      2500, // initial release portion in Bips 100ths of 1%
-      ninetyDaysInSeconds // period between releases in seconds
-    )
-
+    await createReleaseSchedule(hre, reserveAccount, tokenLockup.address)
     expect(await tokenLockup.scheduleCount()).to.equal(4)
+
+    expect((await tokenLockup.releaseSchedules(0)).releaseCount).to.equal('5')
+    expect((await tokenLockup.releaseSchedules(0)).delayUntilFirstReleaseInSeconds).to.equal('0')
+    expect((await tokenLockup.releaseSchedules(0)).initialReleasePortionInBips).to.equal('770')
+    expect((await tokenLockup.releaseSchedules(0)).periodBetweenReleasesInSeconds).to.equal(days(90))
+
+    expect((await tokenLockup.releaseSchedules(1)).releaseCount).to.equal('7')
+    expect((await tokenLockup.releaseSchedules(1)).delayUntilFirstReleaseInSeconds).to.equal('0')
+    expect((await tokenLockup.releaseSchedules(1)).initialReleasePortionInBips).to.equal('720')
+    expect((await tokenLockup.releaseSchedules(1)).periodBetweenReleasesInSeconds).to.equal(days(90))
+
+    expect((await tokenLockup.releaseSchedules(2)).releaseCount).to.equal('5')
+    expect((await tokenLockup.releaseSchedules(2)).delayUntilFirstReleaseInSeconds).to.equal('0')
+    expect((await tokenLockup.releaseSchedules(2)).initialReleasePortionInBips).to.equal('2000')
+    expect((await tokenLockup.releaseSchedules(2)).periodBetweenReleasesInSeconds).to.equal(days(90))
+
+    expect((await tokenLockup.releaseSchedules(3)).releaseCount).to.equal('5')
+    expect((await tokenLockup.releaseSchedules(3)).delayUntilFirstReleaseInSeconds).to.equal('0')
+    expect((await tokenLockup.releaseSchedules(3)).initialReleasePortionInBips).to.equal('2500')
+    expect((await tokenLockup.releaseSchedules(3)).periodBetweenReleasesInSeconds).to.equal(days(90))
   })
 })
