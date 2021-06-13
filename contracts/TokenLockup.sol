34bb970e54d7995d105001a968c0851dc1986ad9
Noah Thorp <noah@comakery.com>
2021-06-13 14:11:59 -0700

    Move IERC20Burnable.sol to its own file

--- contracts/TokenLockup.sol
+++ contracts/TokenLockup.sol
@@ -1,15 +1,7 @@
 // SPDX-License-Identifier: MIT
+import "./IERC20Burnable.sol";
 
 pragma solidity 0.8.3;
-
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-// interface with ERC20 and the burn function interface from the associated Token contract
-interface IERC20Burnable is IERC20 {
-    function burn(uint256 amount) external;
-
-    function decimals() external view returns (uint8);
-}
 
 contract TokenLockup {
     IERC20Burnable public token;
