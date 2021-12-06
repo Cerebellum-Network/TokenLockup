638cabb61c83247d333220c8b1c7a109249c4cf7
vayager-gold <eugenemathew93@gmail.com>
2021-12-06 07:31:46 -0500

    Remove unused import

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,7 +2,6 @@
 
 pragma solidity 0.8.3;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
 contract Token is ERC20Capped {
